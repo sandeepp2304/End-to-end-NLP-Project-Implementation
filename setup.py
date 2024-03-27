a666649224d62131d5ee186c34aedba1452c4c8e
sandeepp2304 <sandeepp2304@gmail.com>
2024-03-27 07:34:08 +0530

    Code updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Sandeep",
+    author_email="sandeepp2304@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
